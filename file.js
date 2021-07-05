dc1660389347d931853cf1cb7c49116be04fce44
Kanishk <kanishkrao001@gmail.com>
2021-07-05 12:06:30 +0530

    added some funtions

--- file.js
+++ file.js
@@ -1,7 +1,18 @@
-
+function add(num1, num2)
+{
+    return (num1+num2)
+}
+function mul(a,b)
+{
+    return (a*b)
+}
 function greet(name)
 {
-    console.log("hello" + name)
+    console.log("hello " + name)
 }
 
 greet('kanishk')
+
+console.log(add(5,6))
+
+console.log(mul(1,2))
